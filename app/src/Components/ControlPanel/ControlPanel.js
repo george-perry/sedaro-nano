d37636a9131765e2e3656d38b59b8c5c016c3a38
George <gperry@nd.edu>
2024-03-29 00:07:07 -0400

    Add buttons to increment/decrement frames

--- app/src/Components/ControlPanel/ControlPanel.js
+++ app/src/Components/ControlPanel/ControlPanel.js
@@ -3,19 +3,32 @@ import '../../CSS/ControlPanel.css';
 
 const ControlPanel = ({ currentIndex, setCurrentIndex, maxIndex, showVelocities, setShowVelocities }) => {
     
+    const incrementIndex = () => {
+        // Prevent the index from going too high
+        setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, maxIndex));
+    };
+
+    const decrementIndex = () => {
+        // Prevent the index from going too low
+        setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+    };
+
     return (
-      <div className="slider-container">
-        <h4>Adjust Frame</h4>
-        <input className="slider"
-          type="range"
-          min="0"
-          max={maxIndex}
-          value={currentIndex}
-          onChange={(e) => setCurrentIndex(parseInt(e.target.value, 10))}
-        />
+      <div className="control-panel-container">
+        <h4>Frame {currentIndex}</h4>
+        <div className="slider-container">
+          <button className="button increment" onClick={decrementIndex}>-</button>
+          <input className="slider"
+            type="range"
+            min="0"
+            max={maxIndex}
+            value={currentIndex}
+            onChange={(e) => setCurrentIndex(parseInt(e.target.value, 10))}
+          />
+          <button className="button decrement" onClick={incrementIndex}>+</button>
+        </div>
       </div>
     );
   };
   
-  export default ControlPanel;
-  
+export default ControlPanel;
